b41edbab2cec4b316dd5ee03f58d476796a3ceaf
Syedmoiz7 <syedzada7866@gmail.com>
2023-06-11 20:57:18 +0500

    2nd

---
+++ src/components/index.jsx
@@ -0,0 +1,22 @@
+import './index.css';
+function Design() {
+    return (
+        <div className="main">
+            <div className="sideCont cont">
+                <div className="heading">
+                   <h1>LODGN</h1> 
+                </div>
+                <div className='fourDivs greenfont'>Current Request</div>
+                <div  className='fourDivs'>Ongoing stays</div>
+                <div  className='fourDivs'>Previous stays</div>
+                <div  className='fourDivs'>Reports</div>
+            </div>
+
+            <div className="rightCont cont">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Design;
