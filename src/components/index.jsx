bf43a4a5f0943564d5b1c53a9faf9122da706abc
Syedmoiz7 <syedzada7866@gmail.com>
2023-06-12 12:42:18 +0500

    side container completed

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,19 +3,27 @@ function Design() {
     return (
         <div className="main">
             <div className="sideCont cont">
-                <div className="heading">
-                   <h1>LODGN</h1> 
+                <div className="upperHalf">
+                    <div className="heading">
+                        <h1>LODGN</h1>
+                    </div>
+
+                    <div className='fourDivs greenfont'>Current request</div>
+                    <div className='fourDivs'>Ongoing stays</div>
+                    <div className='fourDivs'>Previous stays</div>
+                    <div className='fourDivs'>Reports</div>
                 </div>
 
-                <div className='fourDivs greenfont'>Current request</div>
-                <div  className='fourDivs'>Ongoing stays</div>
-                <div  className='fourDivs'>Previous stays</div>
-                <div  className='fourDivs'>Reports</div>
-                
+                <div className="bottom">
+                    <button className='btn'>Log-Out</button>
+                    <div>Help-Desk:</div>
+                    <div>786-874 9988</div>
+                </div>
+
             </div>
 
             <div className="rightCont cont">
-                
+
             </div>
         </div>
     )
